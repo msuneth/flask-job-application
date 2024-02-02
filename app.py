d2bbf3985f149ef9680c74a6b37ea6f646970e1b
msuneth <msuneth@gmail.com>
2024-02-02 09:17:44 +0530

    submit form data via post

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
+    return render_template("index.html")
+
+
+app.run(debug=False, port=5001)
